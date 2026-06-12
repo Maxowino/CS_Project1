fa881a21aeabb6f51b5fca20e0d1618d1db5695a
maxowino <maxwellowino27@gmail.com>
2026-06-12 21:59:57 +0300

    scrollable UI

--- lib/screens/selectAction.dart
+++ lib/screens/selectAction.dart
@@ -1,90 +1,188 @@
-// // ignore_for_file: camel_case_types
+// ignore_for_file: camel_case_types
 
 import 'package:animated_background/animated_background.dart';
 import 'package:flutter/material.dart';
-import 'package:cs_project_1/screens/login.dart';
-import 'package:cs_project_1/screens/register.dart';
-import 'package:cs_project_1/screens/adminlogin.dart';
+
+import 'login.dart';
+import 'register.dart';
 
 class selectAction extends StatefulWidget {
   const selectAction({super.key});
 
   @override
-  State<selectAction> createState() => _selectUserState();
+  State<selectAction> createState() =>
+      _selectActionState();
 }
 
-class _selectUserState extends State<selectAction>
+class _selectActionState
+    extends State<selectAction>
     with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
+
       child: Scaffold(
-        backgroundColor: Colors.grey[200],
-
-        appBar: AppBar(
-          title: const Text(
-            'Flood Alert System',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          centerTitle: true,
-
-          actions: [
-            PopupMenuButton(
-              itemBuilder: (context) => [
-                PopupMenuItem(
-                  child: const Text('Admin'),
-                  onTap: () {
-                    Future.delayed(
-                      Duration.zero,
-                      () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) => const adminlogin(),
-                        ),
-                      ),
-                    );
-                  },
-                )
-              ],
-            )
-          ],
-
-          bottom: const TabBar(
-            indicatorColor: Colors.black,
-            labelColor: Colors.black,
-            tabs: [
-              Tab(
-                icon: Icon(Icons.login),
-                text: "Login",
-              ),
-              Tab(
-                icon: Icon(Icons.person_add),
-                text: "Register",
-              ),
-            ],
-          ),
-        ),
+        backgroundColor:
+            Colors.grey.shade200,
 
         body: AnimatedBackground(
           vsync: this,
-          behaviour: RandomParticleBehaviour(
-            options: const ParticleOptions(
-              spawnMaxRadius: 10,
-              spawnMinSpeed: 10,
-              particleCount: 70,
-              spawnMaxSpeed: 40,
-              minOpacity: 0.2,
-              spawnOpacity: 0.3,
-              baseColor: Colors.black,
+
+          behaviour:
+              RandomParticleBehaviour(
+            options:
+                const ParticleOptions(
+              particleCount: 35,
+              spawnMinSpeed: 8,
+              spawnMaxSpeed: 18,
+              minOpacity: 0.1,
+              spawnOpacity: 0.2,
+              baseColor:
+                  Colors.black,
             ),
           ),
 
-          child: const TabBarView(
-            children: [
-              LoginPage(),
-              RegisterPage(),
-            ],
+          child: SafeArea(
+            child: Column(
+              children: [
+
+                const SizedBox(
+                    height: 30),
+
+                const Icon(
+                  Icons.cloud,
+                  size: 70,
+                  color: Colors.black,
+                ),
+
+                const SizedBox(
+                    height: 12),
+
+                // const Text(
+                //   "Flood Alert",
+                //   style: TextStyle(
+                //     fontSize: 24,
+                //     fontWeight:
+                //         FontWeight.bold,
+                //   ),
+                // ),
+
+                // const SizedBox(
+                //     height: 8),
+
+                // Text(
+                //   "Receive localized flood alerts",
+                //   style: TextStyle(
+                //     color:
+                //         Colors.grey[700],
+                //   ),
+                // ),
+
+                const SizedBox(
+                    height: 25),
+
+                Container(
+                  margin:
+                      const EdgeInsets
+                          .symmetric(
+                    horizontal: 24,
+                  ),
+
+                  decoration:
+                      BoxDecoration(
+                    color:
+                        Colors.white,
+                    borderRadius:
+                        BorderRadius
+                            .circular(
+                                30),
+                  ),
+
+                  child:
+                      const TabBar(
+
+                    indicatorSize:
+                        TabBarIndicatorSize.tab,
+                    indicator:
+                        BoxDecoration(
+                      color:
+                          Colors.black,
+                      borderRadius:
+                          BorderRadius
+                              .all(
+                        Radius.circular(
+                            30),
+                      ),
+                    ),
+
+                    labelColor:
+                        Colors.white,
+
+                    unselectedLabelColor:
+                        Colors.black,
+
+                    tabs: [
+                      Tab(
+                        text:
+                            "Login",
+                      ),
+                      Tab(
+                        text:
+                            "Register",
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(
+                    height: 20),
+
+                Expanded(
+                  child:
+                      Container(
+                    margin:
+                        const EdgeInsets
+                            .all(
+                                16),
+
+                    padding:
+                        const EdgeInsets
+                            .all(
+                                10),
+
+                    decoration:
+                        BoxDecoration(
+                      color: Colors
+                          .white,
+
+                      borderRadius:
+                          BorderRadius
+                              .circular(
+                                  20),
+
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors
+                              .black12,
+
+                          blurRadius:
+                              12,
+                        ),
+                      ],
+                    ),
+
+                    child:
+                        const TabBarView(
+                      children: [
+                        LoginPage(),
+                        RegisterPage(),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
