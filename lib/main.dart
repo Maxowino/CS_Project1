e4c6edccf32f6983269d85da5458c4e8e6adfe96
maxowino <maxwellowino27@gmail.com>
2026-06-17 08:12:04 +0300

    mainpage changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:cs_project_1/screens/homepage.dart';
-import 'package:cs_project_1/screens/login.dart';
+
 import 'package:cs_project_1/screens/splashscreen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
